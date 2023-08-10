df4f4556401e92205063d5ca11ea449ab935e367
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-08-10 18:36:20 -0400

    g

--- main.js
+++ main.js
@@ -2,6 +2,74 @@
 
 const generateIntervalButton = document.getElementById('generateIntervalButton');
 const generateScaleButton = document.getElementById('generateScaleButton');
+const numberButtonsContainer = document.getElementById('numberButtons');
+
+
+function bindNumberButtonEvents() {
+    const numberButtons = document.querySelectorAll('.number-button');
+    
+    numberButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const resultInterval = parseInt(document.getElementById('resultInterval').textContent);
+            const resultScale = parseInt(document.getElementById('resultScale').textContent);
+            
+            if (!isNaN(resultInterval) && index === resultInterval) {
+                
+                button.classList.add('correct');
+            } else if (!isNaN(resultScale) && index === resultScale) {
+                
+                button.classList.add('correct');
+            } else {
+              
+                button.classList.add('incorrect');
+            }
+        });
+    });
+}
+//-------------
+function bindNumberButtonEvents() {
+    const numberButtons = document.querySelectorAll('.number-button');
+
+    numberButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const resultInterval = parseInt(document.getElementById('resultInterval').textContent);
+            const resultScale = parseInt(document.getElementById('resultScale').textContent);
+
+            if (!isNaN(resultInterval) && index === resultInterval) {
+               
+                button.classList.add('correct');
+            } else if (!isNaN(resultScale) && index === resultScale) {
+               
+                button.classList.add('correct');
+            } else {
+         
+                button.classList.add('incorrect');
+            }
+
+            setTimeout(() => {
+                button.classList.remove('correct', 'incorrect');
+            }, 1000);
+        });
+    });
+}
+//----------
+
+//----------------------------------------------------------------
+function generateNumberButtons() {
+    const intervalLabels = [
+        "Minor 2nd", "Major 2nd", "Minor 3rd", "Major 3rd",
+        "Perfect 4th", "Dim 5th", "Perfect 5th", "Minor 6th",
+        "Major 6th", "Minor 7th", "Major 7th", "Octave"
+    ];
+
+    for (let i = 0; i <= 11; i++) {
+        const button = document.createElement('button');
+        button.textContent = intervalLabels[i];
+        button.classList.add('number-button'); 
+        numberButtonsContainer.appendChild(button);
+    }
+}
+
 let previousNumber = -1;
 //--------------------------------------------------------------------------------
 function getRandomInterval(min, max) {
@@ -181,5 +249,10 @@ function generateRandomScale() {
     });
 }
 //--------------------
+generateNumberButtons();
+bindNumberButtonEvents();
 generateIntervalButton.addEventListener('click', generateRandomInterval);
 generateScaleButton.addEventListener('click', generateRandomScale);
+
+
+
