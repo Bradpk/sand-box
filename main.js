79dfa67ae7325d664c524a2eeaca3c6a53136904
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-08-10 17:54:12 -0400

    g

--- main.js
+++ main.js
@@ -1,28 +1,185 @@
 // import * as Tone from 'tone'
 
+const generateIntervalButton = document.getElementById('generateIntervalButton');
+const generateScaleButton = document.getElementById('generateScaleButton');
+let previousNumber = -1;
+//--------------------------------------------------------------------------------
+function getRandomInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+function generateRandomInterval() {
+    const min = 0;
+    const max = 11;
+    let randomInterval;
 
-const magicbutton = document.getElementById("magicButton");
+    do {
+        randomInterval = getRandomInterval(min, max);
+    } while (randomInterval === previousNumber);
 
+    previousNumber = randomInterval;
+    document.getElementById('resultInterval').textContent = `${randomInterval}`;
 
-magicbutton.addEventListener("click", async () => {
-    await Tone.start();
-    const synth = new Tone.Synth().toDestination();
-    const now = Tone.now();
-    synth.triggerAttackRelease("C4", "16n", now)
-    synth.triggerAttackRelease("Eb4", "16n", now + 0.3)
-    synth.triggerAttackRelease("G4", "16n", now + 0.6)
-    synth.triggerAttackRelease("G#4", "16n", now + 0.9)
-    synth.triggerAttackRelease("C5", "16n", now + 1.2)
-    synth.triggerAttackRelease("G#4", "16n", now + 2.1)
-    synth.triggerAttackRelease("G4", "16n", now + 2.4)
-    synth.triggerAttackRelease("G#4", "16n", now + 3.3)
-    synth.triggerAttackRelease("G4", "16n", now + 3.6)
-    synth.triggerAttackRelease("G#4", "16n", now + 3.7)
-    synth.triggerAttackRelease("G4", "16n", now + 3.8)
-    synth.triggerAttackRelease("F4", "16n", now + 3.9)
-    synth.triggerAttackRelease("G4", "16n", now + 4.2)
-    synth.triggerAttackRelease("B3", "16n", now + 4.5)
-    synth.triggerAttackRelease("C4", "16n", now + 4.8)
-})
+    Tone.start().then(() => {
+        const synth = new Tone.Synth().toDestination();
+        const now = Tone.now();
 
+        switch (randomInterval) {
+            case 0:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("C#4", "4n", now + 0.5);
+                break;
+            case 1:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("D4", "4n", now + 0.5);
+                break;
+            case 2:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("D#4", "4n", now + 0.5);
+                break;
+            case 3:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("E4", "4n", now + 0.5);
+                break;
+            case 4:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("F4", "4n", now + 0.5);
+                break;
+            case 5:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("F#4", "4n", now + 0.5);
+                break;
+            case 6:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("G4", "4n", now + 0.5);
+                break;
+            case 7:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("G#4", "4n", now + 0.5);
+                break;
+            case 8:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("A4", "4n", now + 0.5);
+                break;
+            case 9:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("A#4", "4n", now + 0.5);
+                break;
+            case 10:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("B4", "4n", now + 0.5);
+                break;
+            case 11:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("C5", "4n", now + 0.5);
+                break;
+
+            default:
+
+                break;
+        }
+    });
+}
+//-----------------------------------------
+function getRandomScale(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRandomScale() {
+    const min = 0;
+    const max = 6;
+    let randomScale;
+
+    do {
+        randomScale = getRandomScale(min, max);
+    } while (randomScale === previousNumber);
+
+    previousNumber = randomScale;
+    document.getElementById('resultScale').textContent = `${randomScale}`;
+
+    Tone.start().then(() => {
+        const synth = new Tone.Synth().toDestination();
+        const now = Tone.now();
+
+        switch (randomScale) {
+            case 0:
+                case 0:
+                    synth.triggerAttackRelease("C4", "4n", now);
+                    synth.triggerAttackRelease("D4", "4n", now + 0.5);
+                    synth.triggerAttackRelease("E4", "4n", now + 1);
+                    synth.triggerAttackRelease("F4", "4n", now + 1.5);
+                    synth.triggerAttackRelease("G4", "4n", now + 2);
+                    synth.triggerAttackRelease("A4", "4n", now + 2.5);
+                    synth.triggerAttackRelease("B4", "4n", now + 3);
+                    synth.triggerAttackRelease("C", "4n", now + 3.5);
+                    break;
+            case 1:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("D4", "4n", now + 0.5);
+                synth.triggerAttackRelease("D#4", "4n", now + 1);
+                synth.triggerAttackRelease("F4", "4n", now + 1.5);
+                synth.triggerAttackRelease("G4", "4n", now + 2);
+                synth.triggerAttackRelease("A4", "4n", now + 2.5);
+                synth.triggerAttackRelease("A#4", "4n", now + 3);
+                synth.triggerAttackRelease("C5", "4n", now + 3.5);
+                break;
+            case 2:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("C#4", "4n", now + 0.5);
+                synth.triggerAttackRelease("D#4", "4n", now + 1);
+                synth.triggerAttackRelease("F4", "4n", now + 1.5);
+                synth.triggerAttackRelease("G4", "4n", now + 2);
+                synth.triggerAttackRelease("G#4", "4n", now + 2.5);
+                synth.triggerAttackRelease("A#4", "4n", now + 3);
+                synth.triggerAttackRelease("C5", "4n", now + 3.5);
+                break;
+            case 3:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("D4", "4n", now + 0.5);
+                synth.triggerAttackRelease("E4", "4n", now + 1);
+                synth.triggerAttackRelease("F#4", "4n", now + 1.5);
+                synth.triggerAttackRelease("G4", "4n", now + 2);
+                synth.triggerAttackRelease("A4", "4n", now + 2.5);
+                synth.triggerAttackRelease("B4", "4n", now + 3);
+                synth.triggerAttackRelease("C5", "4n", now + 3.5);
+                break;
+            case 4:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("D4", "4n", now + 0.5);
+                synth.triggerAttackRelease("E4", "4n", now + 1);
+                synth.triggerAttackRelease("F4", "4n", now + 1.5);
+                synth.triggerAttackRelease("G4", "4n", now + 2);
+                synth.triggerAttackRelease("A4", "4n", now + 2.5);
+                synth.triggerAttackRelease("A#4", "4n", now + 3);
+                synth.triggerAttackRelease("C5", "4n", now + 3.5);
+                break;
+            case 5:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("D4", "4n", now + 0.5);
+                synth.triggerAttackRelease("D#4", "4n", now + 1);
+                synth.triggerAttackRelease("F4", "4n", now + 1.5);
+                synth.triggerAttackRelease("G4", "4n", now + 2);
+                synth.triggerAttackRelease("G#4", "4n", now + 2.5);
+                synth.triggerAttackRelease("A#4", "4n", now + 3);
+                synth.triggerAttackRelease("C5", "4n", now + 3.5);
+                break;
+            case 6:
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("C#4", "4n", now + 0.5);
+                synth.triggerAttackRelease("D#4", "4n", now + 1);
+                synth.triggerAttackRelease("F4", "4n", now + 1.5);
+                synth.triggerAttackRelease("F#4", "4n", now + 2);
+                synth.triggerAttackRelease("G#4", "4n", now + 2.5);
+                synth.triggerAttackRelease("A#4", "4n", now + 3);
+                synth.triggerAttackRelease("C5", "4n", now + 3.5);
+                break;
+
+            default:
+
+                break;
+        }
+    });
+}
+//--------------------
+generateIntervalButton.addEventListener('click', generateRandomInterval);
+generateScaleButton.addEventListener('click', generateRandomScale);
